689e9c8ecda233c0d953a3612e533cd499f4339d
Aluno <Aluno@ETEJK-LAB04-M10>
2023-11-07 20:19:35 -0300

    Subindo tudo

--- pdv/src/main/java/br/com/pdv/controller/LojaController.java
+++ pdv/src/main/java/br/com/pdv/controller/LojaController.java
@@ -1,14 +1,23 @@
 package br.com.pdv.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.pdv.repository.LojaRepository;
 
 @Controller
 public class LojaController {
 
+	@Autowired
+	private LojaRepository lojaRepository; //cria uma instância do nosso repositório
+	
 	@GetMapping ({"/lojas"}) //É o nome que eu escolher
-	public String home(String username) {
+	public String home(ModelMap model) {
 		
+		//o findAll lista todas lojas
+		model.addAttribute("lojas", lojaRepository.findAll());
 		return "lojas"; // é o nome do arquivo real
 	}
 	
